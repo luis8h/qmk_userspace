a497571b2c6f35a275ffeacc42f700d5f93fa88b
luis8h <vzbls@t-online.de>
2025-03-27 09:40:10 +0100

    finished tapdance implementation

--- keyboards/ferris/sweep/keymaps/luis8h/keymap.c
+++ keyboards/ferris/sweep/keymaps/luis8h/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "tapdance_helper.h"
 #include "i18n.h"
 #if __has_include("keymap.h")
     #include "keymap.h"
@@ -78,6 +79,11 @@
 #define D_TR_2() OSL(L_SYM)
 
 
+// define tap dance keys
+enum {
+    DANCE_1,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_BASE] = LAYOUT_split_3x5_2(
         D_L1_1(KC_Q), D_L1_2(KC_W), D_L1_3(KC_E), D_L1_4(KC_R), D_L1_5(KC_T),                   D_R1_1(KC_Y), D_R1_2(KC_U), D_R1_3(KC_I), D_R1_4(KC_O), D_R1_5(KC_P),
@@ -95,9 +101,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         D_L1_1(KC_EXLM), D_L1_2(KC_AT), D_L1_3(KC_HASH), D_L1_4(KC_DLR), D_L1_5(KC_PERC),       D_R1_1(KC_CIRC), D_R1_2(KC_AMPR), D_R1_3(KC_LPRN), D_R1_4(KC_RPRN), D_R1_5(KC_ASTR),
         D_L2_1(KC_MINS), D_L2_2(KC_QUOT), D_L2_3(KC_LCBR), D_L2_4(KC_RCBR), D_L2_5(KC_GRV),     D_R2_1(KC_PLUS), D_R2_2(KC_SLSH), D_R2_3(KC_DQUO), D_R2_4(KC_COLN), D_R2_5(),
         D_L3_1(KC_UNDS), D_L3_2(KC_SCLN), D_L3_3(KC_LBRC), D_L3_4(KC_RBRC), D_L3_5(KC_EQL),     D_R3_1(KC_TILD), D_R3_2(KC_BSLS), D_R3_3(KC_COMM), D_R3_4(KC_DOT), D_R3_5(),
-        D_TL_1(), D_TL_2(),                                                                     D_TR_1(), D_TR_2()
+        DANCE_1, D_TL_2(),                                                                      D_TR_1(), D_TR_2()
     ),
-    [L_NUMT] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LGUI, KC_NO, LCTL(KC_LALT), LCA(KC_LSFT), KC_TRNS, KC_HOME, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [L_NUMT] = LAYOUT_split_3x5_2(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_DOWN, KC_RGHT, KC_TRNS,                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,                                                                       KC_TRNS, KC_TRNS
+    ),
     [L_SYMSPEC] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PIPE, KC_QUOT, KC_TRNS, KC_CIRC, KC_ASTR, KC_AMPR, KC_NO, KC_TRNS, KC_HASH, KC_TILD, KC_SLSH, KC_DQUO, KC_DLR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_BSLS, KC_GRV, KC_TRNS, RM_PREV, KC_TRNS, KC_TRNS, RM_NEXT),
     [L_NUMH] = LAYOUT_split_3x5_2(KC_TRNS, KC_COLN, KC_LT, KC_GT, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_AT, KC_TRNS, KC_NO, KC_EQL, KC_PLUS, KC_PERC, KC_TRNS, KC_EXLM, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_TRNS, KC_TRNS, KC_VOLU),
     [L_WORKA] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, KC_NO, LCTL(KC_LALT), KC_TRNS, KC_TRNS, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_F11, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
@@ -107,74 +118,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_SYS] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_COLN, KC_ESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_PERC, KC_SLSH, KC_ENT, KC_TRNS, DF(1), KC_LGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_EXLM, KC_TRNS, DF(0), KC_TRNS, RALT_T(KC_COMM), RCTL_T(KC_DOT), QK_BOOT, KC_TRNS, KC_TAB, KC_NO, KC_TRNS)
 };
 
-
-// 1. Define your tap dance identifiers.
-enum {
-    TD_LAYER_CHANGE, // index 0 for our tap dance key
-    // ... add more if needed
-};
-
-// 2. Define tap dance step types for clarity.
-typedef enum {
-    SINGLE_TAP,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_SINGLE_TAP
-} tap_dance_step_t;
-
-// 3. Global state for tap dance; adjust array size if you use multiple keys.
-typedef struct {
-    tap_dance_step_t step;
-} dance_state_t;
-dance_state_t dance_state[1];
-
-// 4. Helper function to determine tap dance step.
-int dance_step(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) {
-            return SINGLE_TAP;
-        } else {
-            return SINGLE_HOLD;
-        }
-    }
-    if (state->count == 2) {
-        return DOUBLE_TAP;
-    }
-    return 0;
-}
-
-// 5. Finished function: execute action based on tap dance type.
-void dance_3_finished(tap_dance_state_t *state, void *user_data) {
-    // Use index 0 for our single tap dance key.
+// tap dance 1 functions
+void dance_1_finished(tap_dance_state_t *state, void *user_data) {
     dance_state[0].step = dance_step(state);
     switch (dance_state[0].step) {
         case SINGLE_TAP:
-            layer_move(0);
+            layer_move(L_BASE);
             break;
         case SINGLE_HOLD:
-            layer_on(7);
+            layer_on(L_ESC);
             break;
         case DOUBLE_TAP:
-            layer_move(0);
+            layer_move(L_BASE);
             break;
         case DOUBLE_SINGLE_TAP:
-            layer_move(0);
+            layer_move(L_BASE);
             break;
     }
 }
-
-// 6. Reset function: clean up state after tap dance finishes.
-void dance_3_reset(tap_dance_state_t *state, void *user_data) {
+void dance_1_reset(tap_dance_state_t *state, void *user_data) {
     wait_ms(10);
     if (dance_state[0].step == SINGLE_HOLD) {
-        layer_off(7);
+        layer_off(L_ESC);
     }
     dance_state[0].step = 0;
 }
 
-// 7. Register the tap dance action using the updated type and macro.
+// register tap dance keys
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_LAYER_CHANGE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_3_finished, dance_3_reset)
+    [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset)
 };
 
 
---
+++ keyboards/ferris/sweep/keymaps/luis8h/tapdance_helper.h
@@ -0,0 +1,26 @@
+typedef enum {
+    SINGLE_TAP,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_SINGLE_TAP
+} tap_dance_step_t;
+
+typedef struct {
+    tap_dance_step_t step;
+} dance_state_t;
+dance_state_t dance_state[1];
+
+int dance_step(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) {
+            return SINGLE_TAP;
+        } else {
+            return SINGLE_HOLD;
+        }
+    }
+    if (state->count == 2) {
+        return DOUBLE_TAP;
+    }
+    return 0;
+}
+
