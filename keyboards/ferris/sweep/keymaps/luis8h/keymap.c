5a26eabf94fec05f0a025d84a6b5eff03216de81
Luis Schmidmeister <luis8h@Luiss-MacBook-Pro.local>
2025-04-23 13:54:03 +0200

    Revert "small fix"
    This reverts commit d494f1398124676cde7be19f1a203cbcba63671c.

--- keyboards/ferris/sweep/keymaps/luis8h/keymap.c
+++ keyboards/ferris/sweep/keymaps/luis8h/keymap.c
@@ -329,7 +329,7 @@ tap_dance_action_t tap_dance_actions[] = {
     [DANCE_10] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_10, dance_10_finished, dance_10_reset),
     [DANCE_11] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_11, dance_11_finished, dance_11_reset),
     [DANCE_12] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_12, dance_12_finished, dance_12_reset),
-    [DANCE_13] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_13, dance_13_finished, dance_13_reset),
+    [DANCE_12] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_13, dance_13_finished, dance_13_reset),
 };
 
 
