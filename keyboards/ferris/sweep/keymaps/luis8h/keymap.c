d477ad09135e25e9bb0ac308915fcce2d554fb5e
Luis Schmidmeister <luis8h@Luiss-MacBook-Pro.local>
2025-04-23 11:29:57 +0200

    testing

--- keyboards/ferris/sweep/keymaps/luis8h/keymap.c
+++ keyboards/ferris/sweep/keymaps/luis8h/keymap.c
@@ -331,19 +331,24 @@ void dance_13_finished(tap_dance_state_t *state, void *user_data) {
     dance_state[12].step = dance_step(state);
     switch (dance_state[12].step) {
         case SINGLE_TAP:
-            if (get_mods() & MOD_BIT(KC_LCTL) && (current_os == OS_MACOS || current_os == OS_IOS)) {
-                // 1) release physical Ctrl
-                unregister_code16(KC_LCTL);
-                // 2) press Alt
-                register_code16(KC_LALT);
-                // 3) tap Right-arrow
+            // Only on macOS/iOS when Ctrl is held:
+            if ((get_mods() & MOD_BIT(KC_LCTL))
+                && (current_os == OS_MACOS || current_os == OS_IOS)) {
+
+                // 1) Physically release Ctrl
+                unregister_mods(MOD_BIT(KC_LCTL));
+                // 2) Physically press Alt
+                register_mods(MOD_BIT(KC_LALT));
+                // 3) Tap the Right-Arrow
                 tap_code16(KC_RIGHT);
-                // 4) release Alt
-                unregister_code16(KC_LALT);
-                // 5) restore Ctrl
-                register_code16(KC_LCTL);
+                // 4) Release Alt
+                unregister_mods(MOD_BIT(KC_LALT));
+                // 5) Restore Ctrl
+                register_mods(MOD_BIT(KC_LCTL));
+
             } else {
-                register_code16(KC_RIGHT);
+                // Fallback: normal arrow tap
+                tap_code16(KC_RIGHT);
             }
             break;
         case SINGLE_HOLD:
